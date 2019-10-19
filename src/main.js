e4b851d989837601ebd2f404ddb2ca5d55c4dfe2
aslan-alt <54422571+aslan-alt@users.noreply.github.com>
2019-10-20 00:28:39 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -82,16 +82,16 @@ body{
     background: radial-gradient(circle, rgba(253,187,45,1) 0%, rgba(253,187,45,1) 20%, rgba(34,193,195,1) 20%, rgba(34,193,195,1) 100%);
 }
 #div2 {
-        animation:move  5s  linear  infinite 
+    animation:move 5s linear infinite 
+}
+@keyframes move {
+    from {
+        transform: rotate(0);
     }
-@keyframes  move {
-        from  {
-            transform:  rotate(0);
-        }
-        to  {
-            transform:  rotate(360deg);
-        }
+    to {
+        transform: rotate(360deg);
     }
+}
 `
 let n = 0
 let string_print = ''
